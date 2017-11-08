a9b24acf35d1b0b8864a9d6c25809fdf36356923
Eleve O3W <croascent@gmail.com>
2017-11-08 13:33:39 +0100

    delete old

--- Cystopia_old/jeu/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('MYSQL_HOST', 'localhost');
-    define('MYSQL_NAME', 'cystopia');
-    define('MYSQL_USER', 'root');
-    define('MYSQL_PASS', 'root');
--- Cystopia_old/landingpage/admin/path.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-echo realpath('path.php'); 
-
-
-
-
-?>
